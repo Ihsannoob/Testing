e839c42f139754e7a640a98e499758734c4526e4
Ihsannoob <88837514+Ihsannoob@users.noreply.github.com>
2025-09-12 07:32:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
   alert('Selamat datang kawan di server Minecraft kami 😆!');
 });
+const menuToggle = document.getElementById('menuToggle');
+const dropdownMenu = document.getElementById('dropdownMenu');
+
+menuToggle.addEventListener('click', () => {
+  if (dropdownMenu.style.display === 'block') {
+    dropdownMenu.style.display = 'none';
+  } else {
+    dropdownMenu.style.display = 'block';
+  }
+});
+
+// Klik di luar menu untuk menutup
+document.addEventListener('click', function(event) {
+  if (!menuToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
+    dropdownMenu.style.display = 'none';
+  }
+});
